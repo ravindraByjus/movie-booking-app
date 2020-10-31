d8a08df83022bde6ce440e4b7dd8377c328523a4
Ravindra <ravindra.varma@byjus.com>
2020-10-31 12:27:32 +0530

    movie booking app build

---
+++ client/build/precache-manifest.c7a00eca5740e744be65933dfb2a23b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69cba267c8fcc1e60c64b659845305f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e44519d9e515473a302",
+    "url": "/static/css/2.a6a636a0.chunk.css"
+  },
+  {
+    "revision": "b95a8deb7489454247a3",
+    "url": "/static/css/main.4f1f98a7.chunk.css"
+  },
+  {
+    "revision": "1e44519d9e515473a302",
+    "url": "/static/js/2.423bce39.chunk.js"
+  },
+  {
+    "revision": "610359362e2f9d5fa420eb531608a3c9",
+    "url": "/static/js/2.423bce39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b95a8deb7489454247a3",
+    "url": "/static/js/main.61d41abb.chunk.js"
+  },
+  {
+    "revision": "386c0847a0d97386d5a2",
+    "url": "/static/js/runtime-main.fc74f1cf.js"
+  }
+]);
